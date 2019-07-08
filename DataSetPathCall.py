d197be1be8d32df720902b4bc0afc87201fb0353
soichiro310 <s183332@edu.tut.ac.jp>
2019-07-08 11:59:32 +0900

    add DataSetPathCall.py

---
+++ DataSetPathCall.py
@@ -0,0 +1,67 @@
+import os.path
+import pprint
+
+class DataSetPathCall():
+    def __init__(self, pathlist=['','','','']):
+        self.__homeDir = os.path.expanduser("~") + '/'
+        self.__frameDir = pathlist[0]
+        self.__trainDir = pathlist[0] + pathlist[1]
+        self.__testDir = pathlist[0] + pathlist[2]
+        
+        if pathlist[3] is '' :
+            self.__validationDir = ''
+        else :
+            self.__validationDir = pathlist[0] + pathlist[3]
+        
+        self.__pathlist = [self.__frameDir, self.__trainDir, self.__testDir, self.__validationDir]
+    
+    def setPath(self,pathlist):
+        self.__frameDir = pathlist[0]
+        self.__trainDir = pathlist[0] + pathlist[1]
+        self.__testDir = pathlist[0] + pathlist[2]
+        
+        if pathlist[3] is '' :
+            self.__validationDir = ''
+        else :
+            self.__validationDir = pathlist[0] + pathlist[3]
+        
+        self.__pathlist = [self.__frameDir, self.__trainDir, self.__testDir, self.__validationDir]
+    
+    def getHomeDir(self):
+        return self.__homeDir
+    
+    def getFrameDir(self):
+        return self.__frameDir
+    
+    def getTrainDir(self):
+        return self.__trainDir
+    
+    def getTestDir(self):
+        return self.__testDir
+    
+    def getValidationDir(self):
+        return self.__validationDir
+    
+    def printPathList(self):
+        pprint.pprint(self.__pathlist)
+    
+    def definePath(self, path):
+        return self.getHomeDir() + path
+
+
+class UCF101_PathCall(DataSetPathCall):
+    def __init__(self):
+        super().__init__()        
+        pathlist = [
+            self.getHomeDir() + 'dataset/UCF101/UCF-101_frame',
+            '/train',
+            '/test',
+            ''
+        ]
+        self.setPath(pathlist)
+        
+            
+if __name__ == '__main__':
+    a = UCF101_PathCall()
+    a.printPathList()
+    
