387f37fc0add92c405c947e771be6564d49af747
soichiro310 <s183332@edu.tut.ac.jp>
2019-07-16 16:17:30 +0900

    add printAccuracyList Func

--- ActionRecognitionModels/trainlog.py
+++ ActionRecognitionModels/trainlog.py
@@ -22,6 +22,22 @@ def plot(training_log):
         plt.plot(cnn_benchmark)
         plt.show()
 
+def printAccuracyList(training_log):
+    with open(training_log) as fin:
+        reader = csv.reader(fin)
+        next(reader, None)  # skip the header
+        accuracies = []
+        top_5_accuracies = []
+        cnn_benchmark = []  # this is ridiculous
+        for epoch,acc,loss,top_k_categorical_accuracy,val_acc,val_loss,val_top_k_categorical_accuracy in reader:
+            #accuracies.append(float(acc))
+            accuracies.append(float(val_acc))
+            top_5_accuracies.append(float(val_top_k_categorical_accuracy))
+            cnn_benchmark.append(0.65)  # ridiculous
+        
+        print(max(accuracies))
+        print(max(top_5_accuracies))
+
 if __name__ == '__main__':
     training_log = 'data/logs/mlp-training-1489455559.7089438.log'
     main(training_log)
